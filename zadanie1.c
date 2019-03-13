028e85f7eb155a50418ce3d802148b762ef6f600
krisolew <41170897+krisolew@users.noreply.github.com>
2019-03-14 00:40:22 +0100

    Update zadanie1.c

--- zadanie1.c
+++ zadanie1.c
@@ -11,7 +11,22 @@ int main(int argc, char* argv[]){
     int opt;
     int index = 1, size, current_index=0, delete_index;
     char *dir, *file, *tmp_file;
-    char ** table;
+    char **table;
+    int i=0;
+
+    struct timespec start, stop;
+    double duration;
+    clock_t start3, stop3;
+    static struct tms st_cpu;
+    static struct tms en_cpu;
+
+    clock_t start2, end;
+    double cpu_time_used;
+
+    start2 = clock();
+
+    clock_gettime(CLOCK_REALTIME, &start);
+    start3 = times(&st_cpu);
 
     while ((opt=getopt(argc,argv,"c:f:d:")) != -1)
     {
@@ -19,7 +34,16 @@ int main(int argc, char* argv[]){
         switch(opt)
         {
             case 'c':
-                size = (int) optarg[0] - '0';
+                size = 0;
+                while (optarg[i]!='\0')
+                {
+                    size*=10;
+                    size += (int) optarg[i] - '0';
+                    i++;
+                }
+
+                printf("%d \n", size);
+                
                 table = create_table(size);
                 break;
             case 'f':
@@ -28,8 +52,10 @@ int main(int argc, char* argv[]){
                 file = strdup(argv[index++]);
                 tmp_file = strdup(argv[index]);
 
+                
                 find(dir, file, tmp_file);
-                save(table, current_index);
+
+                save(table,current_index);
                 break;
             case 'd':
                 delete_index = (int) optarg[0] - '0';
@@ -39,24 +65,20 @@ int main(int argc, char* argv[]){
         index++;
     }
 
-    // char ch;
-    // int i = 0;
-    // while ( (ch = dir[i]) != '\0' ){
-    //     printf("%c", ch);
-    //     i++;
-    // }
-    // printf("\n");
-    // i = 0;
-    // while ( (ch = file[i]) != '\0' ){
-    //     printf("%c", ch);
-    //     i++;
-    // }
-    //     printf("\n");
-    // i = 0;
-    // while ( (ch = tmp_file[i]) != '\0' ){
-    //     printf("%c", ch);
-    //     i++;
-    // }    printf("\n");
+    stop3 = times(&en_cpu);
+    clock_gettime(CLOCK_REALTIME, &stop);
+
+    end = clock();
+    cpu_time_used = ((double) (end - start2)) / CLOCKS_PER_SEC;
+
+    duration = stop.tv_sec - start.tv_sec + (stop.tv_nsec - start.tv_nsec) * 1.0/1000000000;
+    printf ("Real time %lf s\n", duration);
+    printf ("Cpu user time %Lf s\n", (long double)(en_cpu.tms_utime - st_cpu.tms_utime)/sysconf(_SC_CLK_TCK));
+    printf ("Cpu system time %Lf s\n", (long double)(en_cpu.tms_stime - st_cpu.tms_stime)/sysconf(_SC_CLK_TCK));
+    printf("Real: %jd \n", stop3 - start3);
+    printf("System: %f \n", (double) en_cpu.tms_stime - st_cpu.tms_stime);
+    printf("User: %f \n", (double) en_cpu.tms_utime - st_cpu.tms_utime);
+    printf("%f \n", cpu_time_used);
     
     return 0;
 }
