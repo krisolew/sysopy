a938b66309260ba9c8c0cdcb2fc65a857613f9ab
krisolew <krisolew@localhost.localdomain>
2019-03-14 17:38:56 +0100

    writing to file added

--- zestaw1/main.c
+++ zestaw1/main.c
@@ -5,6 +5,58 @@
 #include<string.h>
 #include<time.h>
 #include<sys/times.h>
+#define PLIK "raport2.txt"
+
+struct timespec start, stop;
+static struct tms st_cpu;
+static struct tms en_cpu;
+ 
+void measure_start_time()
+{
+    clock_gettime(CLOCK_REALTIME, &start);
+    times(&st_cpu);
+}
+
+void measure_stop_time()
+{
+    times(&en_cpu);
+    clock_gettime(CLOCK_REALTIME, &stop);
+}
+
+void print_and_save_times(char parametr)
+{
+    FILE* file = fopen(PLIK,"a");
+    
+    switch(parametr){
+        case 'f':
+            printf("wywołanie funkcji find: \n");
+            fprintf(file, "wywołanie funkcji find: \n");
+            break;
+        case 'd':
+            printf("wywołanie funkcji delete: \n");
+            fprintf(file, "wywołanie funkcji delete: \n");
+            break;
+        case 'c':
+            printf("wywołanie funkcji create_table: \n");
+            fprintf(file, "wywołanie funkcji create_table: \n");
+            break;
+    }
+
+    double duration = stop.tv_sec - start.tv_sec + (stop.tv_nsec - start.tv_nsec) * 1.0/1000000000;
+    long double cpu_user_time = (long double)(en_cpu.tms_utime - st_cpu.tms_utime)/sysconf(_SC_CLK_TCK);
+    long double cpu_system_time = (long double)(en_cpu.tms_stime - st_cpu.tms_stime)/sysconf(_SC_CLK_TCK);
+
+    printf("Real time %lf s\n", duration);
+    printf("Cpu user time %Lf s\n", cpu_user_time);
+    printf("Cpu system time %Lf s\n", cpu_system_time);
+    printf("\n");
+
+    fprintf(file, "Real time %lf s\n", duration);
+    fprintf(file, "Cpu user time %Lf s\n", cpu_user_time);
+    fprintf(file, "Cpu system time %Lf s\n", cpu_system_time);
+    fprintf(file, "\n");
+    fclose(file);
+}
 
 int main(int argc, char* argv[]){
 
@@ -13,16 +65,7 @@ int main(int argc, char* argv[]){
     char *dir, *file, *tmp_file;
     char **table;
     int i=0;
-
-    struct timespec start, stop;
-    double duration;
-    static struct tms st_cpu;
-    static struct tms en_cpu;
-
-    clock_gettime(CLOCK_REALTIME, &start);
-    times(&st_cpu);
-
-    int liczba_opcji = 0;
+    
     while ((opt=getopt(argc,argv,"c:f:d:")) != -1)
     {
         index++;
@@ -38,19 +81,26 @@ int main(int argc, char* argv[]){
                     i++;
                 }
                 
+                measure_start_time();
                 table = create_table(size);
+                measure_stop_time();
+                print_and_save_times(opt);
                 break;
+
             case 'f':
         
                 dir = strdup(argv[index++]);
                 file = strdup(argv[index++]);
                 tmp_file = strdup(argv[index]);
 
-                
+                measure_start_time();
                 find(dir, file, tmp_file);
+                measure_stop_time();
+                print_and_save_times(opt);
 
                 save(table,current_index);
                 break;
+
             case 'd':
                 delete_index = 0;
                 i = 0;
@@ -61,21 +111,13 @@ int main(int argc, char* argv[]){
                     i++;
                 }
 
+                measure_start_time();
                 delete(delete_index, table, size);
+                measure_stop_time();
+                print_and_save_times(opt);
                 break;
         }
         index++;
-        liczba_opcji++;
     }
-
-    times(&en_cpu);
-    clock_gettime(CLOCK_REALTIME, &stop);
-
-    duration = stop.tv_sec - start.tv_sec + (stop.tv_nsec - start.tv_nsec) * 1.0/1000000000;
-    printf ("Real time %lf s\n", duration);
-    printf ("Cpu user time %Lf s\n", (long double)(en_cpu.tms_utime - st_cpu.tms_utime)/sysconf(_SC_CLK_TCK));
-    printf ("Cpu system time %Lf s\n", (long double)(en_cpu.tms_stime - st_cpu.tms_stime)/sysconf(_SC_CLK_TCK));
-    printf ("Liczba przekazanych opcji: %d\n", liczba_opcji);
-    
     return 0;
 }
