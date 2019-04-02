d44439bf06a4bc6f9b100aa98836cf81e8f01232
krisolew <krisolew@localhost.localdomain>
2019-04-03 00:14:54 +0200

    cw04  85%

--- OlewinskiKrzysztof/cw04/zad3/catcher.c
+++ OlewinskiKrzysztof/cw04/zad3/catcher.c
@@ -32,26 +32,103 @@ void signal_USR2(int sig_num)
     exit(0);
 }
 
-int main()
+void send_with_queue(int sig_num)
 {
+    union sigval value;
+    value.sival_int = 1;
+
+    if (sender_pid == -1) exit(0);
+    int i = received_signals;
+    while (i> 0)
+    {
+        sigqueue(sender_pid, SIGUSR1, value);
+        i--;
+        value.sival_int++;
+    }
+    sigqueue(sender_pid, SIGUSR2, value);
+    printf("Number of received signals from sender %d\n", received_signals);
+    exit(0);
+}
+
+void signal_2(int sig_num)
+{
+    if (sender_pid == -1) exit(0);
+    int i = received_signals;
+    for (; i>0; i--)
+    {
+        kill(sender_pid, SIGRTMIN+1);
+    }
+
+    kill(sender_pid, SIGRTMIN+2);
+    printf("Number of received signals from sender %d\n", received_signals);
+    exit(0);
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        printf("Expected 1 argument\n");
+        return -1;
+    }
+
+    char *mode = malloc(20 * sizeof(char));
+    mode = argv[1];
+
     sigset_t set, oldset;
     sigemptyset(&oldset);
     sigfillset(&set);
     sigdelset(&set, SIGUSR1);
     sigdelset(&set, SIGUSR2);
+    sigdelset(&set, SIGRTMIN+2);
+    sigdelset(&set, SIGRTMIN+1);
     sigprocmask(SIG_BLOCK, &set, &oldset);
 
     printf("PID catchera: %d\n", getpid());
     
+
     struct sigaction act;
     sigemptyset(&act.sa_mask);
-    act.sa_flags = SA_SIGINFO;
-    sigaddset(&act.sa_mask, SIGUSR2);
-    act.sa_sigaction = &count_function;
-    sigaction(SIGUSR1, &act, NULL);
-    act.sa_flags = 0;
-    act.sa_handler = signal_USR2;
-    sigaction(SIGUSR2, &act, NULL);
+    
+   
+    if ( strcmp(mode,"KILL") == 0)
+    {
+        act.sa_flags = SA_SIGINFO;
+        sigaddset(&act.sa_mask, SIGUSR2);
+        act.sa_sigaction = &count_function;
+        sigaction(SIGUSR1, &act, NULL);
+        act.sa_flags = 0;
+        act.sa_handler = signal_USR2;
+        sigaction(SIGUSR2, &act, NULL);
+    }
+    else if ( strcmp(mode,"SIGQUEUE") == 0)
+    {
+        act.sa_flags = SA_SIGINFO;
+        act.sa_sigaction = &count_function;
+        sigemptyset(&act.sa_mask);
+        sigaddset(&act.sa_mask, SIGUSR2);
+        sigaction(SIGUSR1, &act, NULL);
+        act.sa_flags = 0;
+        act.sa_handler = send_with_queue;
+        sigaction(SIGUSR2, &act, NULL);
+
+    }
+    else if ( strcmp(mode,"SIGRT") == 0)
+    {
+        act.sa_flags = SA_SIGINFO;
+        sigaddset(&act.sa_mask, SIGRTMIN+2);
+        act.sa_sigaction = &count_function;
+        sigaction(SIGRTMIN+1, &act, NULL);
+        act.sa_flags = 0;
+        act.sa_handler = signal_2;
+        sigaction(SIGRTMIN+2, &act, NULL);
+    }
+    else
+    {
+        printf("Incorrect mode argument\n");
+        return -1;
+    }
+
 
     while(1) {
         sleep(1);
--- OlewinskiKrzysztof/cw04/zad3/sender.c
+++ OlewinskiKrzysztof/cw04/zad3/sender.c
@@ -13,10 +13,15 @@
 int received_signals = 0;
 int sent_signals;
 
+void count_function(int sig,siginfo_t *info, void *void_ptr)
+{
+    received_signals++;
+    printf("Odebrano sygnal nr %d od catchera \n",info->si_value.sival_int);
+}
+
 void signal_USR1(int sig_num)
 {
     received_signals++;
-    return;
 }
 
 void signal_USR2(int sig_num)
@@ -26,24 +31,49 @@ void signal_USR2(int sig_num)
     exit(0);
 }
 
+void signal_1(int sig_num)
+{
+    received_signals++;
+    return;
+}
+
+void signal_2(int sig_num)
+{
+    printf("Number of received signals %d\n", received_signals);
+    printf("Number of sent signals %d\n", sent_signals);
+    exit(0);
+}
+
+
 void send_with_kill(int num_of_signals, int catcher_pid)
 {
     while (num_of_signals > 0)
     {
-        kill(catcher_pid, SIGUSR1);
+        kill(catcher_pid,SIGUSR1 );
         num_of_signals--;
     }
-    kill(catcher_pid, SIGUSR2);
+    kill(catcher_pid,SIGUSR2 );
 }
 
-void send_with_queue(int num_of_signals, int catcher_pid)
+void send_with_queue(int num_of_signals, int catcher_pid, union sigval value)
 {
-
+    
+    while (num_of_signals > 0)
+    {
+        sigqueue(catcher_pid, SIGUSR1, value);
+        num_of_signals--;
+    }
+    sigqueue(catcher_pid, SIGUSR2, value);
 }
 
 void send_with_sigrt(int num_of_signals, int catcher_pid)
 {
-
+    while (num_of_signals > 0)
+    {
+        kill(catcher_pid, SIGRTMIN+1);
+        num_of_signals--;
+    }
+    kill(catcher_pid, SIGRTMIN+2);
 }
 
 int main(int argc, char* argv[])
@@ -73,24 +103,43 @@ int main(int argc, char* argv[])
     mode = argv[3];
 
     struct sigaction act;
-    act.sa_handler = signal_USR1;
-    sigemptyset(&act.sa_mask);
-    act.sa_flags = 0;
-    sigaddset(&act.sa_mask, SIGUSR2);
-    sigaction(SIGUSR1, &act, NULL);
-    act.sa_handler = signal_USR2;
-    sigaction(SIGUSR2, &act, NULL);
+    union sigval value;
 
     if ( strcmp(mode,"KILL") == 0)
     {
+        act.sa_handler = signal_USR1;
+        sigemptyset(&act.sa_mask);
+        act.sa_flags = 0;
+        sigaddset(&act.sa_mask, SIGUSR2);
+        sigaction(SIGUSR1, &act, NULL);
+        act.sa_handler = signal_USR2;
+        sigaction(SIGUSR2, &act, NULL);
+
         send_with_kill(num_of_signals, catcher_pid);
     }
     else if ( strcmp(mode,"SIGQUEUE") == 0)
     {
-        send_with_queue(num_of_signals, catcher_pid);
+        act.sa_flags = SA_SIGINFO;
+        act.sa_sigaction = &count_function;
+        sigemptyset(&act.sa_mask);
+        sigaddset(&act.sa_mask, SIGUSR2);
+        sigaction(SIGUSR1, &act, NULL);
+        act.sa_flags = 0;
+        act.sa_handler = signal_USR2;
+        sigaction(SIGUSR2, &act, NULL);
+
+        send_with_queue(num_of_signals, catcher_pid, value);
     }
     else if ( strcmp(mode,"SIGRT") == 0)
     {
+        act.sa_handler = signal_1;
+        sigemptyset(&act.sa_mask);
+        act.sa_flags = 0;
+        sigaddset(&act.sa_mask, SIGRTMIN+1);
+        sigaction(SIGRTMIN+1, &act, NULL);
+        act.sa_handler = signal_2;
+        sigaction(SIGRTMIN+2, &act, NULL);
+
         send_with_sigrt(num_of_signals, catcher_pid);
     }
     else
