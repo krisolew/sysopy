e844413e13487ad57f3f78809ffa098527b34587
Krzysztof Olewinski <krisolew@wp.pl>
2019-05-08 18:27:50 +0200

    Added creating and deleting of queue

--- OlewinskiKrzysztof/cw06/zad1/server.c
+++ OlewinskiKrzysztof/cw06/zad1/server.c
@@ -6,8 +6,34 @@
 
 #include "settings.h"
 
+struct Client_t
+{
+    int queueID;
+    int friends[MAX_NUMBER_OF_CLIENTS];
+    int current_friends_number;
+    pid_t pid;
+};
+
+struct Client_t clients[MAX_NUMBER_OF_CLIENTS];
+int queueID = -1;
+
 int main()
 {
-    
+    int i;
+    for (i=0; i < MAX_NUMBER_OF_CLIENTS; i++)
+    {
+        clients[i].queueID = -1;
+        clients[i].current_friends_number = 0;
+    }
+
+    queueID = msgget(getServerQueueKey(), IPC_CREAT | IPC_EXCL | 0666);
+    if (queueID == -1)
+    {
+        perror("Cannot create server queue");
+        return -1;
+    }
+
+    msgctl(queueID, IPC_RMID, NULL);
+
     return 0;
 }
