7169d727f21209ebf0adf2146c137b1966c9204d
Krzysztof Olewinski <krisolew@wp.pl>
2019-05-09 16:12:57 +0200

    Fixed bugs in settings file

--- OlewinskiKrzysztof/cw06/zad1/settings.h
+++ OlewinskiKrzysztof/cw06/zad1/settings.h
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 #define MAX_NUMBER_OF_CLIENTS 20
-#define MEX_MESSAGE_LENGTH 500
+#define MAX_MESSAGE_LENGTH 500
 #define MAX_COMMAND_LENGTH 500
 #define NUMBER_OF_COMMANDS 10
 #define SERVER_KEY 's'
@@ -13,7 +13,7 @@ enum Command_t
     INIT = 2,
     ECHO = 3,
     LIST = 4,
-    FIRENDS = 5,
+    FRIENDS = 5,
     ADD = 6,
     DEL = 7,
     _2ALL = 8,
@@ -25,7 +25,7 @@ struct Message_t
 {
     long msgType;
     pid_t senderId;
-    char msgContent[MEX_MESSAGE_LENGTH];
+    char msgContent[MAX_MESSAGE_LENGTH];
 };
 
 #define MSGSZ sizeof(struct Message_t)-sizeof(long) //msgsz doesn't contain mType
